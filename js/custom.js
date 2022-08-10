8ac4bc60a512d7d0c02d3d64733525c34f4a3031
Md Rannu Ahmed (Riyad) <37493558+rbriyad2@users.noreply.github.com>
2022-08-10 22:50:39 +0600

    Add files via upload
    fasfas

--- js/custom.js
+++ js/custom.js
@@ -91,3 +91,21 @@ $(function(){
 		resetCallback: function() {}
 	});
 });
+
+
+//======back to top button=======
+
+let toTop=document.querySelector(".toTop");
+
+window.addEventListener("scroll",function(){
+    if(window.pageYOffset > 100){
+        toTop.classList.add('active');
+    }else{
+        toTop.classList.remove('active');
+    }
+})
+
+
+
+
+
